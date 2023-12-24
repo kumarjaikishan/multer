5ecedb288be9176799e25507a52d19302917fc23
jai kishan <kumar.jaikishan0@gmail.com>
2023-12-24 13:54:36 +0530

    dffe

--- index.js
+++ index.js
@@ -2,31 +2,61 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const multer = require('multer');
 const upload = require('./middleware/multer_middleware')
 const path = require('path');
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2
 
 app.use(express.json());
 app.use(cors());
+
+cloudinary.config({
+    cloud_name: 'dusxlxlvm',
+    api_key: '214119961949842',
+    api_secret: "kAFLEVAA5twalyNYte001m_zFno"
+});
 
 app.get('/', (req, res) => {
     app.use(express.static(path.resolve(__dirname, 'client', 'dist')))
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
 })
 
-app.post('/photo', upload.single('image'), (req, res) => {
-    // The uploaded file is available in req.file.buffer
-    // You can process it as needed
+app.post('/photo', upload.single('image'), async (req, res) => {
 
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
     }
 
+    try {
+        await cloudinary.uploader.upload(req.file.path, (error, result) => {
+            console.log(error, result);
+            if (error) {
+                return res.status(500).send(error);
+            }
+
+            fs.unlink(req.file.path, (err => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send("error occured while deleting file");
+                }   else {
+                    console.log("file deleted");
+                    return res.status(201).send("File Uploaded");
+                    // Get the files in current directory 
+                    // after deletion 
+                    //   getFilesInDirectory(); 
+                }
+            }));
+        })
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+
+
     // console.log(req.body);  // Contains fields other than files
     // console.log(req.file);  // Contains the uploaded file details
 
     // Process the uploaded file or send a response
-    res.status(200).send('File uploaded successfully.');
+    // res.status(200).send('File uploaded successfully.');
 });
 
 app.listen(port, () => {
